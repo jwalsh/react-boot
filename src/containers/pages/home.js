2630c970093ae0e535cef4d5f8016ec0aa5d4ca1
Jason Walsh <j@wal.sh>
2016-05-25 13:26:11 -0400

    Using standard ES6 examples
    - class esample
    - properties
    - constructor support
    - propTypes
    
    https://facebook.github.io/react/blog/2015/01/27/react-v0.13.0-beta-1.html#mixins

--- src/containers/pages/home.js
+++ src/containers/pages/home.js
@@ -2,11 +2,13 @@
 import React from 'react'
 import { WidgetComponent } from '../../modules/widget/index'
 import { Counter } from '../../modules/counter/index'
+import { HelloMessage } from '../../modules/hello-message'
 
 function Home(props) {
   return <div>
     <h1>react-code Home</h1>
     <WidgetComponent />
+    <HelloMessage name="Sebastian" />
     <Counter />
   </div>
 }
---
+++ src/modules/hello-message.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export class HelloMessage extends React.Component {
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
