78090c3f8906ac4ab90b5bd4bfd666829ba96803
Jason Walsh <j@wal.sh>
2016-05-25 13:27:50 -0400

    ES6 Classes

--- src/modules/counter/index.js
+++ src/modules/counter/index.js
@@ -3,22 +3,19 @@ import React from 'react'
 
 export class Counter extends React.Component {
   constructor(props) {
-    super()
-    this.state = {
-      count: 0,
-    }
+    super(props);
+    this.state = {count: props.initialCount};
   }
-  onClick(e) {
-    this.setState({
-      count: this.state.count + 1,
-    });
+  tick() {
+    this.setState({count: this.state.count + 1});
   }
   render() {
     return (
-        <div>
-        <h2>{this.state.count}</h2>
-        <button onClick={this.onClick.bind(this)}>Count Up!!</button>
-        </div>
-    )
+      <div onClick={this.tick.bind(this)}>
+        Clicks: {this.state.count}
+      </div>
+    );
   }
 }
+Counter.propTypes = { initialCount: React.PropTypes.number };
+Counter.defaultProps = { initialCount: 0 };
